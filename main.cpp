941b6cede235dfd8ca75f5e7f109d9e8b2d3eb33
alexandrkucherov <zinkutal@gmail.com>
2018-03-13 20:04:21 +0700

    origin - main - added MPI test code

--- main.cpp
+++ main.cpp
@@ -2,23 +2,51 @@
 // Created by Александр Кучеров on 06/03/2018.
 //
 
-#include "utils/libs/simpleLogger/simpleLogger.cpp"
 
 #include "Controllers/Methods/AccurateMethod.cpp"
+#include <mpi.h>
 
 using namespace boost;
 
 
 void init() {
     LOG_INFO << "Accurate Method - START";
-    AccurateMethod::init();
+    AccurateMethod accurateMethod(100);
+    accurateMethod.init();
     LOG_INFO << "Accurate Method - END";
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
     LOG_INFO << "Program - START";
     init();
     LOG_INFO << "Program - END";
-    return 0;
+
+    LOG_INFO << "MPI - START";
+    // Initialize the MPI environment
+    MPI_Init(NULL, NULL);
+
+    // Get the number of processes
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // Get the rank of the process
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    // Get the name of the processor
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
+
+    // Print off a hello world message
+    printf("Hello world from processor %s, rank %d"
+           " out of %d processors\n",
+           processor_name, world_rank, world_size);
+
+    // Finalize the MPI environment.
+    MPI_Finalize();
+    LOG_INFO << "MPI - END";
+
+    return EXIT_SUCCESS;
 }
